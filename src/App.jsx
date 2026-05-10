8203ae88f02f1cc793c69e25fbca9b447b44af6f
Ignacio Gutierrez <gutierrezignacio560@gmail.com>
2026-05-09 21:29:56 -0300

    chore: setup inicial con Vite y React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Gestión de Proyectos Educativos</h1>
+      <p>Iniciando migración a React</p>
+    </div>
+  )
+}
+
+export default App
